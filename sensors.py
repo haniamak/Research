b8bf9ab71998560cc32110a07f55bbfd0820e8c6
Hania <hana_mm@op.pl>
2026-05-14 22:21:48 +0200

    add: comparison Poland vs Germany in detection limits, measurment methods, group methods based on station areas

--- sensors.py
+++ sensors.py
@@ -12,9 +12,9 @@ def count_occurance_station_areas(csv_path) -> pd.DataFrame:
     res = (
         df[["Air Quality Station Area", "Air Quality Station Nat Code"]]
         .drop_duplicates()
-        .groupby("Air Quality Station Area", as_index=False)
+        .groupby("Air Quality Station Area")
         .size()
-        .rename(columns={"size": "unique_station_count"})
+        .reset_index(name="unique_station_count")
         .sort_values(
             ["unique_station_count", "Air Quality Station Area"],
             ascending=[False, True],
@@ -36,9 +36,9 @@ def count_cadence_units(csv_path) -> pd.DataFrame:
             ]
         ]
         .drop_duplicates()
-        .groupby("Cadence Unit", as_index=False)
+        .groupby("Cadence Unit")
         .size()
-        .rename(columns={"size": "unique_sampling_points"})
+        .reset_index(name="unique_sampling_points")
         .sort_values(
             ["unique_sampling_points", "Cadence Unit"],
             ascending=[False, True],
@@ -60,11 +60,84 @@ def count_analytical_techniques(csv_path) -> pd.DataFrame:
             ]
         ]
         .drop_duplicates()
-        .groupby("Analytical Technique", as_index=False)
+        .groupby("Analytical Technique")
         .size()
-        .rename(columns={"size": "unique_sampling_points"})
+        .reset_index(name="unique_sampling_points")
         .sort_values(
             ["unique_sampling_points", "Analytical Technique"],
+            ascending=[False, True],
+        )
+        .reset_index(drop=True)
+    )
+
+    return res
+
+
+def cout_detection_limit(csv_path) -> pd.DataFrame:
+    df = pd.read_csv(csv_path)
+
+    res = (
+        df[
+            [
+                "Detection Limit",
+                "Sampling Point Id",
+            ]
+        ]
+        .drop_duplicates()
+        .groupby("Detection Limit")
+        .size()
+        .reset_index(name="unique_sampling_points")
+        .sort_values(
+            ["unique_sampling_points", "Detection Limit"],
+            ascending=[False, True],
+        )
+        .reset_index(drop=True)
+    )
+
+    return res
+
+
+def count_measurement_methods(csv_path) -> pd.DataFrame:
+    df = pd.read_csv(csv_path)
+
+    res = (
+        df[
+            [
+                "Measurement Method",
+                "Sampling Point Id",
+            ]
+        ]
+        .drop_duplicates()
+        .groupby("Measurement Method")
+        .size()
+        .reset_index(name="unique_sampling_points")
+        .sort_values(
+            ["unique_sampling_points", "Measurement Method"],
+            ascending=[False, True],
+        )
+        .reset_index(drop=True)
+    )
+
+    return res
+
+
+def group_method_based_on_area(csv_path) -> pd.DataFrame:
+    df = pd.read_csv(csv_path)
+
+    res = (
+        df[
+            [
+                "Air Quality Station Area",
+                "Measurement Method",
+                "Sampling Point Id",
+            ]
+        ]
+        .drop_duplicates()
+        .groupby(["Air Quality Station Area", "Measurement Method"])
+        .size()
+        .reset_index(name="unique_sampling_points")
+        .sort_values(
+            ["unique_sampling_points", "Air Quality Station Area"],
             ascending=[False, True],
         )
         .reset_index(drop=True)
@@ -94,6 +167,27 @@ def main() -> None:
     print(atG.to_string(index=False))
     print("Poland Analytical Techniques:")
     print(atP.to_string(index=False))
+
+    cdlG = cout_detection_limit(dataGermany)
+    cdlP = cout_detection_limit(dataPoland)
+    print("Germany Detection Limits:")
+    print(cdlG.to_string(index=False))
+    print("Poland Detection Limits:")
+    print(cdlP.to_string(index=False))
+
+    mmG = count_measurement_methods(dataGermany)
+    mmP = count_measurement_methods(dataPoland)
+    print("Germany Measurement Methods:")
+    print(mmG.to_string(index=False))
+    print("Poland Measurement Methods:")
+    print(mmP.to_string(index=False))
+
+    gmbA = group_method_based_on_area(dataGermany)
+    gmbP = group_method_based_on_area(dataPoland)
+    print("Germany Measurement Methods based on Air Quality Station Area:")
+    print(gmbA.to_string(index=False))
+    print("Poland Measurement Methods based on Air Quality Station Area:")
+    print(gmbP.to_string(index=False))
 
 
 if __name__ == "__main__":
